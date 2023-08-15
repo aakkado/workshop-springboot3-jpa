8200cc777ecef77ed127de74bf790f2dcfc731da
aakkado <liltenshidesu@gmail.com>
2023-08-15 14:33:49 -0300

    Product

---
+++ src/main/java/com/aakkado/course/services/ProductService.java
@@ -0,0 +1,25 @@
+package com.aakkado.course.services;
+
+import com.aakkado.course.entities.Product;
+import com.aakkado.course.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    ProductRepository repository;
+
+    public List<Product> findAll(){
+        return repository.findAll();
+    }
+
+    public Product findById(Long id){
+        Optional<Product> obj = repository.findById(id);
+        return obj.get();
+    }
+}
